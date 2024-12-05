793ed336045afa29772256539dce06753f79c4b3
Bilal Taherkheli <bilal76@gmail.com>
2024-12-05 11:03:28 +0100

    day 2 completed

---
+++ test/D02/TC01.cs
@@ -0,0 +1,18 @@
+﻿using aoc.D02;
+
+namespace aoc.test.D02
+{
+  public class TC01
+	{
+    [Theory]
+    [InlineData(2, "initial.txt", false)]
+    [InlineData(639, "input.txt", false)]
+    [InlineData(4, "initial.txt", true)]
+    [InlineData(674, "input.txt", true)]
+    public void D1test(int expected, string input, bool isPartII)
+    {
+      var actual = SafetyChecker.GetSafeRecordsCount(@"./D02/" + input, isPartII);
+      Assert.Equal(expected, actual);
+    }
+  }
+}
