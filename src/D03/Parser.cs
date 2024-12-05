1ab7baae5e25c53c09ac5fbe069b4706f8aaf314
Bilal Taherkheli <bilal76@gmail.com>
2024-12-05 13:50:40 +0100

    day 3 completed

---
+++ src/D03/Parser.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc.D03
+{
+  public static class Parser
+  {
+    public static int Parse(string path, bool isPartII = false)
+    {
+      int sum = 0;
+      var text = File.ReadAllText(path);
+
+      if (isPartII) 
+      {  
+        string pattern = @"do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\)";
+        MatchCollection matches = Regex.Matches(text, pattern);
+        bool enabled = true;
+        
+        foreach (Match match in matches)
+        {
+          if (enabled)
+          {
+            if (!(match.Value == "do()" || match.Value == @"don't()"))            
+              sum += ExecuteMultiply(match.Value);            
+            else
+            {
+              if (match.Value == @"don't()")
+                enabled = false;
+            }        
+          }
+          else //disabled 
+          {
+            if (match.Value == @"do()")
+              enabled = true;
+          }
+        }
+      }
+      else
+      {
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";        
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        foreach (Match match in matches)
+          sum += ExecuteMultiply(match.Value);        
+      }
+
+      return sum;
+    }
+
+    private static int ExecuteMultiply(string value)
+    {
+      var s = value.Trim(new char[] { 'm', 'u', 'l', '(' }).TrimEnd(')');
+      var numbers = s.Split(',');
+      return int.Parse(numbers[0]) * int.Parse(numbers[1]);
+    }
+  }
+}
--- test/D02/TC01.cs
+++ test/D02/TC01.cs
@@ -9,7 +9,7 @@ namespace aoc.test.D02
     [InlineData(639, "input.txt", false)]
     [InlineData(4, "initial.txt", true)]
     [InlineData(674, "input.txt", true)]
-    public void D1test(int expected, string input, bool isPartII)
+    public void D2test(int expected, string input, bool isPartII)
     {
       var actual = SafetyChecker.GetSafeRecordsCount(@"./D02/" + input, isPartII);
       Assert.Equal(expected, actual);
