c61be006d44dd3620c31333d5c17b9a5e98c3e5c
Bilal Taherkheli <bilal76@gmail.com>
2024-12-10 12:33:54 +0100

    completed day-08

---
+++ src/D08/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace aoc.D08
+{
+  public struct Point
+  {
+    public int r;
+    public int c;
+    public bool hasAntenna;
+    public char frequency;
+    public bool isAntinode;
+
+    public Point(int r, int c, bool hasAntenna = false, char frequency = '.')
+    {
+      this.r = r;
+      this.c = c;
+      this.hasAntenna = hasAntenna;
+      this.frequency = frequency;
+    }
+  }
+}
