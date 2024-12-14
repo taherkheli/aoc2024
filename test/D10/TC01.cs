21c6fb70f51644929d9bc6c2aa7e000c92d3e08d
Bilal Taherkheli <bilal76@gmail.com>
2024-12-14 14:24:41 +0100

    completed day 10

---
+++ test/D10/TC01.cs
@@ -0,0 +1,19 @@
+﻿using aoc.D10;
+
+namespace aoc.test.D10
+{
+  public class TC01
+	{
+    [Theory]
+    [InlineData(36, "initial.txt", false)]
+    [InlineData(638, "input.txt", false)]
+    [InlineData(81, "initial-II.txt", true)]
+    [InlineData(1289, "input.txt", true)]
+    public void D10test(long expected, string input, bool isPartII)
+    {
+      var p = new PathFinder(@"./D10/" + input);
+      var actual = p.GetCount(isPartII);
+      Assert.Equal(expected, actual);
+    }
+  }
+}
