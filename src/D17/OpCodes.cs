9ce73429a43f751a8233fe747543df44843444cc
Bilal Taherkheli <bilal76@gmail.com>
2024-12-17 23:17:41 +0100

    completed day 17

---
+++ src/D17/OpCodes.cs
@@ -0,0 +1,14 @@
+﻿namespace aoc.D17
+{
+  public enum OpCodes
+  {
+    ADV = 0,
+    BXL,
+    BST,
+    JNZ,
+    BXC,
+    OUT,
+    BDV,
+    CDV
+  }
+}
---
+++ test/D17/TC01.cs
@@ -0,0 +1,27 @@
+﻿using aoc.D17;
+
+namespace aoc.test.D17
+{
+  public class TC01
+	{
+    [Theory]
+    [InlineData("4,6,3,5,6,3,5,2,1,0", "initial.txt")]
+    [InlineData("7,1,3,4,1,2,6,7,1", "input.txt")]
+    public void D17test(string expected, string input)
+    {
+      var computer = new Computer(@"./D17/" + input);
+      var actual = computer.PartI();
+      Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(117440, "initial-II.txt")]
+    [InlineData(109019476330651, "input.txt")]
+    public void D17test2(long expected, string input)
+    {
+      var computer = new Computer(@"./D17/" + input);
+      var actual = computer.PartII();
+      Assert.Equal(expected, actual);
+    }
+  }
+}
