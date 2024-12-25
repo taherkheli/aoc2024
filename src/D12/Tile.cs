e8e663f82bfc55a950c0ccb622cef033b0bd0bce
Bilal Taherkheli <bilal76@gmail.com>
2024-12-25 12:59:33 +0100

    Completed day 12

---
+++ src/D12/Tile.cs
@@ -0,0 +1,14 @@
+﻿namespace aoc.D12
+{
+  public struct Tile
+  {
+    public (int, int) Pos;
+    public char Type;
+
+    public Tile((int, int) pos, char type)
+    {
+      Pos = pos;
+      Type = type;
+    }
+  }
+}
